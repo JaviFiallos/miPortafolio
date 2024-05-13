06295672992cdbfa0032750fd63d97afbe2e0b2f
JaviF <fiallosedisonreggae@gmail.com>
2024-05-13 11:20:40 -0500

    Se agrego la seccion Skills en tarjetas de presentacion, se dio estilos a esas tarjetas

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skills.css";
 import { SKILLS } from "../../utils/data";
 import SkillsCard from "./SkillsCard/SkillsCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
 
 const Skills = () => {
+
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelectedSkill = (data) => {
+    setSelectedSkill(data);
+  }
+
   return (
     <section className="skills-container">
       <h5>Habilidades Técnicas</h5>
@@ -16,13 +24,20 @@ const Skills = () => {
                 key={item.title}
                 iconURL={item.icon}
                 title={item.title}
+                isActive={selectedSkill.title === item.title}
+                onClick={()=>{
+                  handleSelectedSkill(item);
+                }}
               />
             );
           })}
         </div>
 
         <div className="skills-info">
-            
+            <SkillsInfoCard
+              heading={selectedSkill.title}
+              skills={selectedSkill.skills}
+            />
         </div>
       </div>
     </section>
